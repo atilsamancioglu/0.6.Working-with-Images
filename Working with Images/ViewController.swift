a463b219a381cfcf34fd08c572638d48d2bb09b8
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-01-15 15:22:35 +0300

    final commit

--- Working with Images/ViewController.swift
+++ Working with Images/ViewController.swift
@@ -10,16 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var number = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        number = 0
+        nameLabel.text = "Cartman"
+        
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.changeTheName))
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    @IBAction func changeClicked(_ sender: Any) {
+        
+        //button for changinc the image
+        
+        if number == 0 {
+            imageView.image = UIImage(named: "kenny.png")
+            nameLabel.text = "Kenny"
+            number = number + 1
+        } else {
+            imageView.image = UIImage(named: "cartman.png")
+            nameLabel.text = "Cartman"
+            number = number - 1
+        }
+
+    }
+    
+    func changeTheName() {
+        
+        //function for changing the name
+        
+        if number == 0 {
+            imageView.image = UIImage(named: "kenny.png")
+            nameLabel.text = "Kenny"
+            number = number + 1
+        } else {
+            imageView.image = UIImage(named: "cartman.png")
+            nameLabel.text = "Cartman"
+            number = number - 1
+        }
+
+        
+    }
+    
+    
 
 }
 
